84601fd039f93ee057362721551a7acf1fa63657
Marcin <pagodemc@gmail.com>
2016-10-02 16:13:56 +0200

    basic user bundle

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -16,4 +16,13 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return $this->render('main/index.html.twig');
     }
+
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function adminAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('admin/index.html.twig');
+    }
 }
